982c73d32f2241a6309604578ea2c0e63fab0c72
lunaGatica <89168675+lunaGatica@users.noreply.github.com>
2022-11-28 21:09:51 -0300

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,20 @@
+
+function saludar() {
+    console.log("Bienvenido")
+    let nombre = prompt("Ingresa tu nombre");
+    alert("Bienvenido" + nombre);
+    
+    console.log("Bienvenido")
+    let apellido = prompt("Ingresa tu apellido");
+    alert("Bienvenido" + nombre + apellido);
+
+    console.log("Bienvenido")
+    let comidaFavorita = prompt("Ingresa tu comida Favorita" + nombre);
+    alert("Tu comida favorita es" + comidaFavorita);
+}
+
+
+
+
+
+ 
